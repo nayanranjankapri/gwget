2007a56f1976f50fcc61cd74c5f6d1131fd4af72
David Sedeño <david@alderia.com>
2005-07-24 11:58:03 +0000

    Set ftp_proxy env var when use gnome proxy.
    2005-07-23  David Sedeño <david@alderia.com>
    
            * src/gwget_data.c: Set ftp_proxy env var when use gnome proxy.

--- src/gwget_data.c
+++ src/gwget_data.c
@@ -439,6 +439,7 @@ gwget_data_start_download(GwgetData *gwgetdata)
 			if ( strcmp (gwget_pref.network_mode, "default" ) == 0 && gwget_pref.gnome_http_proxy && gwget_pref.gnome_use_proxy) 
 			{
 				setenv("http_proxy",g_strconcat(g_strdup_printf("%s",gwget_pref.gnome_http_proxy),":",g_strdup_printf("%d",gwget_pref.gnome_http_proxy_port),NULL),1);
+				setenv("ftp_proxy",g_strconcat(g_strdup_printf("%s",gwget_pref.http_proxy),":",g_strdup_printf("%d",gwget_pref.http_proxy_port),NULL),1);
 	            		argv[arg]="-Yon";
 				arg++;
 			}
