8f678dc84734cc626bf2b8a91250c1e50a03d4cf
David Sedeño <david@alderia.com>
2008-01-08 18:20:45 +0000

    Rename configure.in to configure.ac Use po/LINGUAS Update
    2008-01-08  David Sedeño <david@alderia.com>
    
            * Rename configure.in to configure.ac
            * Use po/LINGUAS 
            * autogen.sh: Update
    
    
    svn path=/trunk/; revision=566

--- autogen.sh
+++ autogen.sh
@@ -14,23 +14,7 @@ PKG_NAME="gwget"
 	DIE=1
 }
 
-AUTOMAKE=automake-1.7
-ACLOCAL=aclocal-1.7
-
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-        AUTOMAKE=automake
-        ACLOCAL=aclocal
-}
-
-($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
-
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -d $srcdir/src \
     && test -f $srcdir/src/gwget_data.c) || {
         echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
@@ -38,7 +22,14 @@ ACLOCAL=aclocal-1.7
 	exit 1
 }
 
-gnome_autogen=`which gnome-autogen.sh`
-test -z "$gnome_autogen"
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
 
-USE_GNOME2_MACROS=1 . $gnome_autogen
+REQUIRED_AUTOCONF_VERSION=2.57
+REQUIRED_AUTOMAKE_VERSION=1.7
+REQUIRED_INTLTOOL_VERSION=0.35.0
+REQUIRED_PKG_CONFIG_VERSION=0.16.0
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
